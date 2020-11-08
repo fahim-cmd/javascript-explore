864dbdb9958adac53e27b6d08b5fef5cd2b18fe0
fahim-cmd <hfahim087@gmail.com>
2020-11-08 22:09:34 +0600

    double and triple equal done

---
+++ Advanced javascript/double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+// let age = 2;                                        
+// let dadAge = '2';
+
+// let num = 1;
+// let ans = true;
+
+let num = 0;
+let ans = false;
+
+if ( num == ans){
+    console.log(" condition is true");
+}
+else{
+    console.log("condition is false");
+}
+// Triple equal dile variable er typs o check kore ejonno exect ta dibe;
+// double equal a ki typs er variable ta check kore na . kono rokom mil paileii hoe;
---
+++ Advanced javascript/null-vs-undefined.js
@@ -0,0 +1,15 @@
+// const person = { name: "mahbub", phone: 123242, id:545646};
+// console.log(person.address);
+
+// const person = [ 2, 3, 4, 6];
+// console.log(person[11]);
+
+// let name ;
+// console.log(name);
+
+function add ( num, num1){
+    let result = num + num1;
+    return
+}
+const sum = add( 12 , 12);
+console.log(sum);
