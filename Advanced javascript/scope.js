aa150a79edc88cd0b6762d0d1478b324d052dba8
fahim-cmd <hfahim087@gmail.com>
2020-11-09 12:26:42 +0600

    scope

---
+++ Advanced javascript/scope.js
@@ -0,0 +1,10 @@
+var tomCruse = 200;
+function global (){
+    var arnold = 100;
+    console.log(arnold);
+    function local(){
+        var shakib = 300;
+        console.log(shakib + arnold + tomCruse);
+    }
+
+}
